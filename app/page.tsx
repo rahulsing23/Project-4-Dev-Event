4ff8f179626c58450e97404336b44254ea977ef9
Rahul Singh <rahul561singh@gmail.com>
2026-03-22 15:15:04 +0530

    Final commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import ExploreBtn from "@/components/ExploreBtn";
 import EventCart from '@/components/EventCard';
 import events from "@/lib/constants";
@@ -25,7 +26,7 @@ const page = async () => {
       <div className="mt-20 space-y-7" id="events">
         <h3>Featured Events</h3>
         <ul className="events">
-          {events && events.length > 0 && events.map((event) => (
+          {events && events.length > 0 && events.map((event: IEvent) => (
             <li key={event.title} className="list-none">
               <EventCart {...event}/>
             </li>
