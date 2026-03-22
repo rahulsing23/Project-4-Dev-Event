8e3e1c993c4e90fe6ff3b74a5cbb7cbdf4f2e0d2
Rahul Singh <rahul561singh@gmail.com>
2026-03-22 15:05:21 +0530

    api routes

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,8 @@ const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 const page = async () => {
 
-  const response = await fetch(`${BASE_URL}/api/events`);
-  const { events } = await  response.json();
+  // const response = await fetch(`${BASE_URL}/api/events`);
+  // const { events } = await  response.json();
   return (
     <section>
       <h1 className="text-center">
@@ -22,7 +22,7 @@ const page = async () => {
       <div className="mt-20 space-y-7" id="events">
         <h3>Featured Events</h3>
         <ul className="events">
-          {events && events.length > 0 && events.map((event: IEvent) => (
+          {events && events.length > 0 && events.map((event) => (
             <li key={event.title} className="list-none">
               <EventCart {...event}/>
             </li>
