e30478c5f86f8989fc248048ef99b65bd91f3193
Rahul Singh <rahul561singh@gmail.com>
2026-03-22 15:22:37 +0530

    Final commit

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -2,14 +2,27 @@
 
 import Event from '@/database/event.model';
 import connectDB from "@/lib/mongodb";
+import { unstable_cache } from "next/cache";
 
-export const getSimilarEventsBySlug = async (slug: string) => {
+export const getSimilarEventsBySlug = unstable_cache(
+  async (slug: string) => {
     try {
-        await connectDB();
-        const event = await Event.findOne({ slug });
+      await connectDB();
 
-        return await Event.find({ _id: { $ne: event._id }, tags: { $in: event.tags } }).lean();
+      const event = await Event.findOne({ slug });
+
+      if (!event) return [];
+
+      const similarEvents = await Event.find({
+        _id: { $ne: event._id },
+        tags: { $in: event.tags }
+      }).lean();
+
+      return similarEvents;
     } catch {
-        return [];
+      return [];
     }
-}
+  },
+  ["similar-events"], // cache key
+  { revalidate: 60 }  // cache for 60 sec
+);
