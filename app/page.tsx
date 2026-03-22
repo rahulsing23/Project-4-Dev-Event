8c1d3f187713edcdb3e0913594bd6c3b2fb19f39
Rahul Singh <rahul561singh@gmail.com>
2026-03-22 15:10:46 +0530

    Final commit

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,8 @@ const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 const page = async () => {
 
-  // const response = await fetch(`${BASE_URL}/api/events`);
-  // const { events } = await  response.json();
+   const response = await fetch(`${BASE_URL}/api/events`);
+   const { events } = await  response.json();
   return (
     <section>
       <h1 className="text-center">
