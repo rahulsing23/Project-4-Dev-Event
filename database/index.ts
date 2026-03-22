d48a35de2aaf7fe57b5b6d12f141c57da102c677
Rahul Singh <rahul561singh@gmail.com>
2026-03-22 12:47:27 +0530

    api routes

--- database/index.ts
+++ database/index.ts
@@ -4,4 +4,4 @@ export { default as Booking } from './booking.model';
 
 // TypeScript interfaces exports
 export type { IEvent } from './event.model';
-export type { IBooking } from './booking.model';
+// export type { IBooking } from '@database/booking.model.ts';
--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -13,7 +13,7 @@ declare global {
 }
 
 const MONGODB_URI = process.env.MONGODB_URI;
-
+console.log(MONGODB_URI)
 
 // Initialize the cache on the global object to persist across hot reloads in development
 let cached: MongooseCache = global.mongoose || { conn: null, promise: null };
